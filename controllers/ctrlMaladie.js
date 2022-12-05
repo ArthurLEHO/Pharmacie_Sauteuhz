a841aa23d2ff0c977cb25cac2da9fa915d11ce89
Arthur <arthur.leho@gmail.com>
2022-12-05 20:23:07 +0100

    CRUD Maladies

--- controllers/ctrlMaladie.js
+++ controllers/ctrlMaladie.js
@@ -1,20 +1,119 @@
 const modelMaladie = require('../models/modelMaladie');
 
 const controllerMaladie = {
-	
-	async affichageMaladie(req, res){
+
+	async affichageMaladie(req, res) {
 
 		try {
 
 			const data = await modelMaladie.Maladies.afficherMaladies()
 
-			if(data){
+			if (data) {
 
-				res.render("maladie", {dataMaladie: data})
+				res.render("maladie", { dataMaladie: data })
 
-			}else{
+			} else {
 
-				res.render("maladie", {dataMaladie: {} })
+				res.render("maladie", { dataMaladie: {} })
+			}
+
+		} catch (error) {
+
+			console.log(error)
+		}
+	},
+
+	async affichageUneMaladie(req, res) {
+
+		try {
+
+			const data = await modelMaladie.Maladies.afficherMaladies(req)
+
+			if (data) {
+
+				res.render("modifierMaladie", { dataMaladie: data })
+
+			} else {
+
+				res.render("maladieMaladie", { dataMaladie: {} })
+			}
+
+		} catch (error) {
+
+			console.log(error)
+		}
+	},
+
+	redirectionMaladie(req, res) {
+
+		try {
+
+			res.render("ajouterMaladie")
+
+		} catch (error) {
+
+			console.log(error)
+		}
+	},
+
+	async ajouterMaladie(req, res) {
+
+		try {
+
+			const data = await modelMaladie.Maladies.ajouterMaladie(req)
+
+			if (data) {
+
+				res.redirect("/maladie");
+
+			} else {
+
+				console.log("champs incorrects")
+				res.render("ajouterMaladie");
+			}
+
+		} catch (error) {
+
+			console.log(error)
+		}
+	},
+
+	async supprimerMaladie(req, res) {
+
+		try {
+
+			const data = await modelMaladie.Maladies.supprimerMaladie(req)
+
+			if (data) {
+
+				res.redirect("/maladie");
+
+			} else {
+
+				console.log("erreur lors de l'ajout");
+				res.redirect("/maladie");
+			}
+
+		} catch (error) {
+
+			console.log(error)
+		}
+	},
+
+	async modifierMaladie(req, res) {
+
+		try {
+
+			const data = await modelMaladie.Maladies.modifierMaladie(req)
+
+			if (data) {
+
+				res.redirect("/maladie");
+
+			} else {
+
+				console.log("champs incorrects")
+				res.redirect("/maladie/modifierMaladie/" + req.params.id);
 			}
 
 		} catch (error) {
--- routes/routesMaladie.js
+++ routes/routesMaladie.js
@@ -1,7 +1,12 @@
-const express =  require('express');
+const express = require('express');
 const maladieCtrl = require('../controllers/ctrlMaladie.js');
-const router = express.Router() ;
+const router = express.Router();
 
 router.get('/', maladieCtrl.affichageMaladie);
+router.get('/ajouter', maladieCtrl.redirectionMaladie);
+router.post('/ajouterMaladie', maladieCtrl.ajouterMaladie);
+router.get('/supprimerMaladie/:id', maladieCtrl.supprimerMaladie);
+router.get('/modifierMaladie/:id', maladieCtrl.affichageUneMaladie);
+router.post('/modifierMaladie/:id', maladieCtrl.modifierMaladie);
 
-module.exports = router 
+module.exports = router
