4909946d70ecd20436e966a85cfb60face2cee61
Arthur <arthur.leho@mail.com>
2022-12-06 16:53:43 +0100

    Mise en place du certificat (https)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const ejs = require('ejs')
 const path = require('path')
+const fs = require('fs')
+const https = require('https')
+const port = 3000
 
 const mainRoutes = require('./routes/routesMain.js')
 const ordonnancesRoutes = require('./routes/routesOrdonnance.js')
@@ -10,6 +13,10 @@ const medicamentsRoutes = require('./routes/routesMedicament.js')
 const mutuellesRoutes = require('./routes/routesMutuelle.js')
 const maladiesRoutes = require('./routes/routesMaladie.js')
 
+const key = fs.readFileSync(path.join(__dirname, 'certificate', 'server.key'));
+const cert = fs.readFileSync(path.join(__dirname, 'certificate', 'server.cert'));
+const options = { key, cert };
+
 
 let app = express()
 app.set('view engine', 'ejs')
@@ -18,7 +25,9 @@ app.use(express.static('public'))
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.listen(3000);
+https.createServer(options, app).listen(port, () => {
+    console.log(`server running HTTPS. Go to https://localhost:${port}`);
+    }); 
 
 //Définition des routes
 app.use('/', mainRoutes)
