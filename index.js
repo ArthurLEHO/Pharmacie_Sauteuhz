baf5c1e53ece43b8e6d21ad6f9c4ed91d7cf117e
Arthur <arthur.leho@gmail.com>
2022-12-15 22:26:08 +0100

    CSS Page Authentification

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ let app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('views'))
 app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 app.use(express.json());
 app.use(express.urlencoded());
